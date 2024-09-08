3a53b289e77b9e6a717cf7f79e84e19e2d2b89b0
FindMalek <malekgarahellalbus@gmail.com>
2024-09-08 22:18:26 +0100

    feat: linked SDK with DB

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,24 @@
+import { db } from "@/src/prisma"
 import { ClientProviderSettings } from "@/src/types"
 import { createOpenAI, OpenAIProvider } from "@ai-sdk/openai"
 
-export const createUndrstnd = (
+export async function createUndrstnd(
   options?: ClientProviderSettings
-): OpenAIProvider => {
+): Promise<OpenAIProvider> {
+  const api_token = await db.aPIToken.findFirst({
+    where: {
+      id: options?.apiKey,
+      deletedAt: null,
+    },
+  })
+
+  if (!api_token) {
+    throw new Error("API token not found")
+  }
+  
   return createOpenAI({
     baseURL: process.env.GROQ_API_ENDPOINT,
+    apiKey: api_token.token,
     ...options,
   })
 }
