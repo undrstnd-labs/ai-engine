6bbe7732f3d2cb2d9f43b82ec27eb576694fd9e3
FindMalek <malekgarahellalbus@gmail.com>
2024-09-09 19:34:23 +0100

    fix: resolved 'body' in GET

--- src/client.ts
+++ src/client.ts
@@ -11,9 +11,6 @@ export async function createUndrstnd(
         "Content-Type": "application/json",
         "x-api-key": options?.apiKey as string,
       },
-      body: JSON.stringify({
-        apiKey: options?.apiKey,
-      }),
     }
   )
 
