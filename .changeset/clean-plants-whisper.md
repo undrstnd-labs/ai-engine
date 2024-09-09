679c7a3f6a409a0f8e5eabb79655349a9500bd73
FindMalek <malekgarahellalbus@gmail.com>
2024-09-09 19:53:54 +0100

    fix: resolving GET 'token'

---
+++ .changeset/clean-plants-whisper.md
@@ -0,0 +1,5 @@
+---
+"@undrstnd/ai-engine": patch
+---
+
+Console logging error
--- src/client.ts
+++ src/client.ts
@@ -4,17 +4,17 @@ import { createOpenAI, OpenAIProvider } from "@ai-sdk/openai"
 export async function createUndrstnd(
   options?: ClientProviderSettings
 ): Promise<OpenAIProvider> {
-  const token = await fetch(
-    `https://dev.undrstnd-labs.com/api/token`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": options?.apiKey as string,
-      },
-    }
-  )
+  const token = await fetch(`https://dev.undrstnd-labs.com/api/token`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": options?.apiKey as string,
+    },
+  })
+  console.log("Response object:", token)
 
   const apiKey = await token.json()
+  console.log("Response object text:", await token.text())
 
   if (!apiKey) {
     throw new Error("API token not found")
